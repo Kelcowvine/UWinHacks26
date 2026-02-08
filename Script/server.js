679038bd7e580b23ac54ad7a484196f8ffb5834a
Wilson Tran <wilsontran2017@gmail.com>
2026-02-08 15:52:06 -0500

    DIE

--- Script/server.js
+++ Script/server.js
@@ -7,19 +7,41 @@ const PORT = 3000;
 http.createServer((req, res) => {
     if (req.url === "/") {
         // Serve index.html (assume it's in project/)
-        const htmlPath = path.join(__dirname, "index.html");
+        const htmlPath = path.join(__dirname, "financial_report_display.html");
         const html = fs.readFileSync(htmlPath, "utf8");
         res.writeHead(200, { "Content-Type": "text/html" });
         res.end(html);
 
-    } else if (req.url === "/data") {
+    } else if (req.url === "/data/msft") {
         // Serve JSON file from jsonData/
         const jsonPath = path.join(__dirname, "..", "jsonData", "MSFT.json");
         const jsonData = fs.readFileSync(jsonPath, "utf8");
         res.writeHead(200, { "Content-Type": "application/json" });
         res.end(jsonData);
 
-    } else {
+    }
+    else if (req.url === "/data/aapl") {
+        const jsonPath = path.join(__dirname, "..", "jsonData", "AAPL.json");
+        const jsonData = fs.readFileSync(jsonPath, "utf8");
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(jsonData);
+    }
+
+    else if (req.url === "/data/amzn") {
+        const jsonPath = path.join(__dirname, "..", "jsonData", "AMZN.json");
+        const jsonData = fs.readFileSync(jsonPath, "utf8");
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(jsonData);
+    }
+
+    else if (req.url === "/data/goog") {
+        const jsonPath = path.join(__dirname, "..", "jsonData", "GOOG.json");
+        const jsonData = fs.readFileSync(jsonPath, "utf8");
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(jsonData);
+    }
+
+    else {
         res.writeHead(404, { "Content-Type": "text/plain" });
         res.end("Not found");
     }
