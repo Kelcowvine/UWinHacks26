2250d57c92ab9e3f402f8b47d6a9044edb2873ce
Wilson Tran <wilsontran2017@gmail.com>
2026-02-08 13:56:29 -0500

    working script ig wtf

--- Script/nodeJS.js
+++ Script/nodeJS.js
@@ -2,14 +2,23 @@ const fs = require("fs");
 
 console.log("Node is running");
 // 1️⃣ Read the file as text
-const text = fs.readFileSync("../jsonData/APPL.json", "utf8");
+const text = fs.readFileSync("../jsonData/AMZN.json");
 
 // 2️⃣ Parse JSON into a JS object/array
 const data = JSON.parse(text);
 
-console.log(data);
 
-// Example: loop through items
-data.forEach(item => {
-    console.log(`${item.name} costs ${item.price}`);
-});
+
+for (const item of data) {
+    const values = Object.values(item);
+    const firstNum = values[0];
+    const second = values[1];
+
+    if (second == null){
+        console.log(firstNum)
+        continue;
+    }
+    console.log(`\t${firstNum}, ${second}`)
+
+}
+
